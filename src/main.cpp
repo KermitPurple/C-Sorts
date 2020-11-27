27736fecd988a10260b12ddac0baa12962872411
Shane McDonough <shanemcdman@gmail.com>
2020-11-27 02:12:25 -0800

    Impliment merge sort

--- src/main.cpp
+++ src/main.cpp
@@ -34,12 +34,52 @@ void selectionsort(T* arr, const int& size){
     }
 }
 
+template<class T>
+void merge(T* arr, const int& start, const int& middle, const int& end){
+    T* left = new T[middle - start];
+    for(int i = start; i < middle; i++){
+        left[i - start] = arr[i];
+    }
+    T* right = new T[end - middle];
+    for(int i = middle; i < end; i++){
+        right[i - middle] = arr[i];
+    }
+    int li = 0; // left index
+    int ri = 0; // right index
+    for(int i = start; i < end; i++){
+        if(li < middle - start && ri >= end - middle) // if only items are in the left
+            arr[i] = left[li++];
+        else if(ri < end - middle && li >= middle - start) // if only items are in the right
+            arr[i] = right[ri++];
+        else if(li < middle - start && ri < end - middle){ // if items are in right and left
+            if(left[li] < right[ri])
+                arr[i] = left[li++];
+            else
+                arr[i] = right[ri++];
+        }
+    }
+    delete[] left;
+    delete[] right;
+}
+
+template<class T>
+void mergesort(T* arr, const int& start, const int& end){
+    if((end - start) <= 1) return;
+    int middle = (end + start) / 2;
+    mergesort(arr, start, middle);
+    mergesort(arr, middle, end);
+    merge(arr, start, middle, end);
+}
+
 int main(){
+
     const int SIZE = 8;
     int arr[SIZE] = {10, 5, 3, 8, 9, 4, 11, 6};
     print_arr(arr, SIZE);
     // bubblesort(arr, SIZE); // works
-    selectionsort(arr, SIZE);
+    // selectionsort(arr, SIZE); // works
+    mergesort(arr, 0, SIZE); // works
     print_arr(arr, SIZE);
+
     return 0;
 }
